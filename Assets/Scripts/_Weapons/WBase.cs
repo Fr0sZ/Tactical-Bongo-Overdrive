ca69fb4991a8de5c58eecc58f1d5cf2792b6cf18
bubblefrog <a95@live.se>
2013-11-28 19:33:30 +0100

    ForceGit FIX

--- Assets/Scripts/_Weapons/WBase.cs
+++ Assets/Scripts/_Weapons/WBase.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WBase : MonoBehaviour {
-
+public abstract class WBase : MonoBehaviour {
+	public Sprite WTexture;
+	public float  FireSpeed;
 	private abstract void Start();
 	private abstract void Update();
 	public abstract void Fire();
