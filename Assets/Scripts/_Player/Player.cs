6fb4e6ed4f4939e5c1c3593a0d571fbb505aa0d4
Fr0sZ <victormoller.95@gmail.com>
2013-12-02 22:26:19 +0100

    Added a base for the player to use weapons

--- Assets/Scripts/_Player/Player.cs
+++ Assets/Scripts/_Player/Player.cs
@@ -2,13 +2,31 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-	
+
+	private WBase m_weapon;
+
 	void OnTriggerEnter2D(Collider2D collider)
 	{
 		PBase powerup;
 		if(powerup = collider.GetComponent<PBase>())
 		{
 			powerup.OnPickUp(this.gameObject);
+		}
+	}
+
+	public void OnHit()
+	{
+	}
+
+	public void Attack()
+	{
+		if(m_weapon)
+		{
+			m_weapon.Fire();
+		}
+		else
+		{
+
 		}
 	}
 }
