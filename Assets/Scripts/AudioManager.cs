d59f607b68923dd17bf1b8a9fe61b86873bbcbfc
Fr0sZ <victormoller.95@gmail.com>
2013-12-02 19:22:59 +0100

    Added a audiomanager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour 
+{	
+	public void PlaySoundEffect(AudioClip soundEffect)
+	{
+		AudioSource audioSource = gameObject.AddComponent<AudioSource>() as AudioSource;
+		audioSource.clip = soundEffect;
+		audioSource.Play();
+		Destroy(audioSource, soundEffect.length);
+	}
+
+	public void PlaySoundEffect(AudioClip soundEffect, float volume)
+	{
+		AudioSource audioSource = gameObject.AddComponent<AudioSource>() as AudioSource;
+		audioSource.clip = soundEffect;
+		audioSource.Play();
+		audioSource.volume = volume;
+		Destroy(audioSource, soundEffect.length);
+	}
+}
