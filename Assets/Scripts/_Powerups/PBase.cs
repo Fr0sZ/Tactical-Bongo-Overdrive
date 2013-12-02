c702df501b288ea2cf794e984c3d6f638717307f
Fr0sZ <victormoller.95@gmail.com>
2013-12-02 19:23:16 +0100

    Added powerup base script

---
+++ Assets/Scripts/_Powerups/PBase.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PBase : MonoBehaviour 
+{
+	public AudioClip m_pickUpAudioClip;
+
+	public virtual void OnPickUp(GameObject player)
+	{
+		Camera.main.GetComponent<AudioManager>().PlaySoundEffect(m_pickUpAudioClip);
+		Destroy(this.gameObject);
+	}
+}
