f0668fe8cdcde95606b5e3d22cb6f5d7e4d2f8bc
Fr0sZ <victormoller.95@gmail.com>
2013-12-02 21:26:33 +0100

    Added player pickup for powerups

---
+++ Assets/Scripts/_Player/Player.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		PBase powerup;
+		if(powerup = collider.GetComponent<PBase>())
+		{
+			powerup.OnPickUp(this.gameObject);
+		}
+	}
+}
