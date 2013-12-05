96e8d637728da38c942a18038df11837850d62f5
Xingez <xing.lichfrozen@gmail.com>
2013-12-05 16:43:55 +0100

    Hud Add

---
+++ Assets/Scripts/_GUI/guiHud.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class guiHud : MonoBehaviour {
+
+
+
+	// Ini Health
+	void Start () {
+
+	}
+
+	// Update Health, current weapon + powerup
+	void Update () {
+		
+	}
+
+	public Texture2D icon;
+	void OnGUI ()  {
+		GUI.Box(new Rect (10,Screen.height - 60,100,50), icon);
+	}
+}
