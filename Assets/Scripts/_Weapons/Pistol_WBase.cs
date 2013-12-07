2be56237b5866e1ab8c9f0f79b20c295aafe3e11
bubblefrog <a95@live.se>
2013-12-07 16:24:28 +0100

    Fixed Pistol

--- Assets/Scripts/_Weapons/Pistol_WBase.cs
+++ Assets/Scripts/_Weapons/Pistol_WBase.cs
@@ -14,5 +14,4 @@ public class Pistol_WBase : WBase {
 		
 	}
 	
-	
 }
--- Assets/Scripts/_Weapons/WBase.cs
+++ Assets/Scripts/_Weapons/WBase.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 
 public abstract class WBase : MonoBehaviour {
+
+	protected Vector3 m_WeaponOffset = new Vector3(0.7F,-0.2F,0);
+
 	public float  FireCooldown;
 	public int Ammo;
 	public abstract void Fire();
@@ -16,4 +19,28 @@ public abstract class WBase : MonoBehaviour {
 		}
 		return false;
 	}
+
+	public AudioClip m_pickUpAudioClip;
+	
+	public virtual void OnPickUp(GameObject player)
+	{
+		PlayPickUpSound();
+		
+		Destroy(GetComponent<Rigidbody2D>());
+		Destroy(GetComponent<BoxCollider2D>());
+		
+		transform.parent = player.transform;
+		transform.localPosition = m_WeaponOffset;
+		transform.localRotation = Quaternion.Euler(0,0,0);
+		
+		player.GetComponent<Player>().Weapon = this;
+	}
+	
+	protected void PlayPickUpSound()
+	{		
+		if(m_pickUpAudioClip)
+			Camera.main.GetComponent<AudioManager>().PlaySoundEffect(m_pickUpAudioClip);
+	}
+
+	
 }
