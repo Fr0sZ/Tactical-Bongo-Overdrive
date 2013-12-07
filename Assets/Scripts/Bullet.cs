69dfdcc4dc9c2f02369f382c7ba5298ad286c514
Fr0sZ <victormoller.95@gmail.com>
2013-12-07 02:28:25 +0100

    Added bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public GameObject m_owner;
+	public int m_damage;
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if(collider.tag == "Player" && m_owner != collider.gameObject)
+		{
+			collider.gameObject.GetComponent<Player>().OnHit(m_owner, m_damage, transform.position, rigidbody2D.velocity);
+			Destroy(gameObject);
+		}
+	}
+}
