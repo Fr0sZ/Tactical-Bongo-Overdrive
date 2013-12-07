e4b6e6db86d675b90207c8021929a32f15153e4f
bubblefrog <a95@live.se>
2013-12-07 15:43:37 +0100

    Added fire code

--- Assets/Scripts/_Weapons/Glock_WBase.cs
+++ Assets/Scripts/_Weapons/Glock_WBase.cs
@@ -2,14 +2,17 @@
 using System.Collections;
 
 public class Glock_WBase : WBase {
-
+	
 	public GameObject Bullet;
 	public override void Fire(){
 		if (CanFire())
-		{}
-			//Fire!!!
-	
+		{
+			GameObject newBullet = Instantiate(Bullet,transform.position,Quaternion.identity) as GameObject;
+			newBullet.rigidbody2D.AddForce(new Vector2(1000,0));
+		}
+		
+		
 	}
-
-
+	
+	
 }
