1b310e5cfa340f4223acc3edb86c537519ad552e
Fr0sZ <victormoller.95@gmail.com>
2013-12-10 05:20:36 +0100

    Improved player collision with small platforms

--- Assets/Scripts/_Player/PlayerMovement.cs
+++ Assets/Scripts/_Player/PlayerMovement.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour 
 {
 	const float ExtraJumpInputTime	= 0.2f; //How long extra time you get for a jump
+	const int 	CollisionRays		= 21;	//How many raycast it should cast to check collision
 
 	public float m_groundSpeed = 8;
 	public float m_airSpeed = 5;
@@ -71,20 +72,19 @@ public class PlayerMovement : MonoBehaviour
 		//Make sure so the player dosen't go through things
 		Vector2 collSize = GetComponent<BoxCollider2D>().size;
 
-		RaycastHit2D hit;
-		RaycastHit2D middleHit	= Physics2D.Raycast(new Vector2(transform.position.x,transform.position.y)  + Mathf.Sign(rigidbody2D.velocity.x) * new Vector2(collSize.x/2 + 0.01f, 0) 			, new Vector2(Mathf.Sign(rigidbody2D.velocity.x),0), 0.01f);
-		RaycastHit2D topHit 	= Physics2D.Raycast(new Vector2(transform.position.x,transform.position.y)  + Mathf.Sign(rigidbody2D.velocity.x) * new Vector2(collSize.x/2 + 0.01f, collSize.y/2) 	, new Vector2(Mathf.Sign(rigidbody2D.velocity.x),0), 0.01f);
-		RaycastHit2D bottomHit	= Physics2D.Raycast(new Vector2(transform.position.x,transform.position.y)  + Mathf.Sign(rigidbody2D.velocity.x) * new Vector2(collSize.x/2 + 0.01f, -collSize.y/2) , new Vector2(Mathf.Sign(rigidbody2D.velocity.x),0), 0.01f);
-
-		hit = middleHit;
-
-		if(!hit && topHit)
+		RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x,transform.position.y)  + Mathf.Sign(rigidbody2D.velocity.x) * new Vector2(collSize.x/2 + 0.01f, 0) 			, new Vector2(Mathf.Sign(rigidbody2D.velocity.x),0), 0.01f);
+	
+		if(!hit)
 		{
-			hit = topHit;
-		}
-		if(!hit && bottomHit)
-		{
-			hit = bottomHit;
+			for(int i = 0; i < CollisionRays; i++)
+			{
+				RaycastHit2D tempHit = Physics2D.Raycast(new Vector2(transform.position.x,transform.position.y)  + Mathf.Sign(rigidbody2D.velocity.x) * new Vector2(collSize.x/2 + 0.01f, -collSize.y/2 + (collSize.y / (CollisionRays-1)) * i) 	, new Vector2(Mathf.Sign(rigidbody2D.velocity.x),0), 0.01f);
+				if(tempHit)
+				{
+					hit = tempHit;
+					break;
+				}
+			}
 		}
 
 		if(hit)	//If you hit something then set your position to the hit point so it dosen't create a gap between you and the wall
