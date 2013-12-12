62b5b20ad925d3e5564716a563a3eb5db4935174
Xingez <xing.lichfrozen@gmail.com>
2013-12-12 20:50:36 +0100

    Grassland: Wall collide fix + Teleport Change

--- Assets/Scripts/GrasslandTeleport.cs
+++ Assets/Scripts/GrasslandTeleport.cs
@@ -7,13 +7,13 @@ public class GrasslandTeleport : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
-		{
+        //if (other.CompareTag("Player"))
+		//{
 			float tempy = spawn.gameObject.transform.position.y; // Change Y to spawn pos
 			float tempx = other.gameObject.transform.position.x; // Use Same X
 			other.gameObject.transform.position = new Vector2(tempx, tempy);
 			Debug.Log("TP: " + other.tag + " X:" + tempx + " Y:" + tempy);
-        }
+        //}
     }
 
 }
