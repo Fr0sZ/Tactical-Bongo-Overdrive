eb955ac727630a5084bcfe9cd292dd2d61055754
Xingez <xing.lichfrozen@gmail.com>
2013-12-16 09:50:39 +0100

    Spawner: Easy change spawn time

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -9,10 +9,14 @@ public class Spawner : MonoBehaviour {
 
 	private float m_nextSpawn;
 	private float m_lastTime;
+
+	public int minSpawn = 2;
+	public int maxSpawn = 20;
+
 	// Use this for initialization
 	void Start () {
 		m_lastTime = Time.timeSinceLevelLoad;
-		m_nextSpawn = Random.Range(2,20);
+		m_nextSpawn = Random.Range(minSpawn,maxSpawn);
 	}
 	
 	// Update is called once per frame
@@ -21,11 +25,10 @@ public class Spawner : MonoBehaviour {
 		if ((Time.timeSinceLevelLoad-m_lastTime)> m_nextSpawn){
 			m_lastTime = Time.timeSinceLevelLoad;
 			float x = m_SpawnArea.transform.localScale.x;
-			float y = m_SpawnArea.transform.localScale.y;
 			float xCord = m_SpawnArea.transform.position.x;
 			float yCord = m_SpawnArea.transform.position.y;
 			GameObject newObject = Instantiate(m_ObjectsToSpawn[Random.Range(0,m_ObjectsToSpawn.Count)], new Vector2(Random.Range(-x/2,x/2)+xCord,yCord), Quaternion.identity) as GameObject;
-			m_nextSpawn = Random.Range(2,20);
+			m_nextSpawn = Random.Range(minSpawn,maxSpawn);
 			Debug.Log (m_nextSpawn);
 		}
 	}
